48ece1d48bdb0ae968818a7023aa9050bf9ba50f
Chris Roos <chris@seagul.co.uk>
2015-07-13 15:40:15 +0100

    Convert outcome_switzerland outcome
    * I've introduced the `ceremony_type_lowercase` helper method to replicate the
    functionality offered by the calculate block of the same name in the
    `partner_opposite_or_same_sex?` question.
    
    * The 'what_you_need_to_do_switzerland_resident_uk', 'switzerland_os_variant',
    'switzerland_ss_variant', 'switzerland_not_resident',
    'switzerland_os_not_resident', 'switzerland_ss_not_resident' and
    'switzerland_not_resident_two' phrases were only used in this outcome so I've
    inlined them rather than introducing partials.
    
    * I've introduced the '_what_you_need_to_do.govspeak.erb' partial as I can see
    that it's going to be used in other templates.

--- test/unit/marriage_abroad_helper_test.rb
+++ test/unit/marriage_abroad_helper_test.rb
@@ -11,5 +11,13 @@ module SmartAnswer
     test '#ceremony_type returns "Civil partnership" for same sex ceremonies' do
       assert_equal 'Civil partnership', ceremony_type('same_sex')
     end
+
+    test '#ceremony_type_lowercase returns "marriage" for opposite sex ceremonies' do
+      assert_equal 'marriage', ceremony_type('opposite_sex')
+    end
+
+    test '#ceremony_type_lowercase returns "civil partnership" for same sex ceremonies' do
+      assert_equal 'civil partnership', ceremony_type('same_sex')
+    end
   end
 end
